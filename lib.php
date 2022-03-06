9164b44f4133196e6392d91993f9b6f6c4ea8e06
Ludwig Richter <riluzm@posteo.de>
2022-03-06 02:26:09 +0100

    fix: Remove absolute paths and replace with runtime determined full paths via `command -v`

--- lib.php
+++ lib.php
@@ -12,33 +12,15 @@ if (!file_exists(CONFIG_PATH)) {
 
 $config = json_decode(file_get_contents(CONFIG_PATH));
 
-/// function section
-
-/**
- * Checks if the configured host is up and running.
- *
- * @return bool <code>true</code> when the host is up
- */
-function is_up(): bool {
-    global $config;
-
-    // send one ICMP ping packet to host
-    // when answered -> host is up
-    // when timeout -> host is down
-    $output = [];
-    $result_code = 0;
-    $cmd = "/usr/bin/ping -q -c 1 -w 1 " . $config->{"host"}->{"ip"};
-    exec($cmd, $output, $result_code);
-
-    return $result_code == 0;
-}
+/// command section
 
 /**
  * Checks if a given command (UNIX binary) is in the PATH environment.
  *
  * @return bool <code>true</code> when the command/binary exists
  */
-function command_exists(string $command): bool {
+function command_exists(string $command): bool
+{
     $output = [];
     $result_code = 0;
     $cmd = "command -v " . $command;
@@ -47,21 +29,64 @@ function command_exists(string $command): bool {
 }
 
 /**
+ * Checks if the given command/binary name exist and returns the path to the executable.
+ *
+ * @param string $command the command to check
+ * @return string the path to the executable
+ */
+function get_command_path(string $command): string
+{
+    $output = [];
+    $result_code = 0;
+    $cmd = "command -v " . $command;
+    exec($cmd, $output, $result_code);
+
+    if ($result_code > 0) {
+        die("Requested command " . $command . " doesn't exist. Please install the required package and try again.");
+    }
+
+    return $output[0];
+}
+
+/// function section
+
+/**
+ * Checks if the configured host is up and running.
+ *
+ * @return bool <code>true</code> when the host is up
+ */
+function is_up(): bool
+{
+    global $config;
+
+    // send one ICMP ping packet to host
+    // when answered -> host is up
+    // when timeout -> host is down
+    $output = [];
+    $result_code = 0;
+    $cmd = get_command_path("ping") . " -q -c 1 -w 1 " . $config->{"host"}->{"ip"};
+    exec($cmd, $output, $result_code);
+
+    return $result_code == 0;
+}
+
+/**
  * Wakes the configured host with a Wake-On-Lan packet.
  *
  * @return bool <code>true</code> when the WoL packet was sent successfully
  */
-function wake(): bool {
+function wake(): bool
+{
     global $config;
 
     // send one WOL packet to the host
-    if (command_exists("/usr/bin/wol")) {
-        $wol_command = sprintf("/usr/bin/wol -i \"%s\" \"%s\"",
+    if (command_exists("wol")) {
+        $wol_command = sprintf(get_command_path("wol") . " -i \"%s\" \"%s\"",
             $config->{"host"}->{"ip"},
             $config->{"host"}->{"mac"}
         );
-    } else if (command_exists("/usr/bin/wakeonlan")) {
-        $wol_command = sprintf("/usr/bin/wakeonlan -i \"%s\" \"%s\"",
+    } else if (command_exists("wakeonlan")) {
+        $wol_command = sprintf(get_command_path("wakeonlan") . " -i \"%s\" \"%s\"",
             $config->{"host"}->{"ip"},
             $config->{"host"}->{"mac"}
         );
